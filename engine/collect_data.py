96b91282014bd1f828e7ea9ddc8b761c3f23208f
Micael Cosmo <micael.trabalho@hotmail.com>
2022-12-03 21:45:16 -0300

    Colunas
    Métodos mostram o primeiro elemento de cada coluna separadamente.

--- engine/collect_data.py
+++ engine/collect_data.py
@@ -1,12 +1,19 @@
 import pandas as pd
-from keys.secret_keys import CURRENT_SHEET_PATH
+from keys.secret_keys import CURRENT_SHEET_SECRETS as CSS
 
 
 class CollectData:
     def __init__(self):
-        self.df = pd.read_excel(CURRENT_SHEET_PATH)
-        print(self.df)
+        data = pd.read_excel(CSS['path'])
+        self.df = pd.DataFrame(data)
+
+    def column_1(self):
+        print(self.df[CSS['column_1']][0])
+
+    def column_2(self):
+        print(self.df[CSS['column_2']][0])
 
 
 if __name__ == '__main__':
-    CollectData()
+    CollectData().column_1()
+    CollectData().column_2()
